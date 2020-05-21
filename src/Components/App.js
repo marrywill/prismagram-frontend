f09a13caa6deff499673647224df03d95d78fb1e
김태인 <marrywill@gimtaein-ui-MacBookAir.local>
2020-05-21 23:30:49 +0900

    200521 | added Router

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../Styles/GlobalStyles';
 import Theme from '../Styles/Theme'
-
+// import Router from './Router'
+import AppRouter from './Router';
 
 export default () => (
   <ThemeProvider theme={Theme}>
-    <GlobalStyles />
+    <>
+      <GlobalStyles />
+      <AppRouter isLoggedIn={false} />
+    </>
   </ThemeProvider>
 );
---
+++ src/Components/Router.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import Feeds from '../Routes/Feeds'
+import Auth from '../Routes/Auth'
+
+const LoggedInRoutes = () => (<><Route exact path="/" component={Feeds}/></>)
+const LoggedOutRoutes = () => (<><Route exact path="/" component={Auth} /></>)
+
+const AppRouter = ({ isLoggedIn}) => (<Router>
+    <Switch>{isLoggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes />}</Switch>
+    </Router>)
+
+AppRouter.propTypes = {
+    isLoggedIn: PropTypes.bool.isRequired
+}
+
+export default AppRouter
