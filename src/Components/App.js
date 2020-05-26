3dace6e99145802727096d56f574b2be69cf8c10
Taein Kim <kti91@likelion.org>
2020-05-26 23:01:59 +0900

    200526 | set up Frontend

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,13 +2,26 @@ import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from '../Styles/GlobalStyles'
 import Theme from '../Styles/Theme'
-// import AppRouter from './Router'
 import Router from './Router'
-export default () => (
-	<ThemeProvider theme={Theme}>
-		<>
-			<GlobalStyles />
-			<Router isLoggedIn={false} />
-		</>
-	</ThemeProvider>
-)
+import { useQuery } from 'react-apollo-hooks'
+import { gql } from 'apollo-boost'
+
+const QUERY = gql`
+	{
+		isLoggedIn @client
+	}
+`
+
+export default () => {
+	const {
+		data: { isLoggedIn },
+	} = useQuery(QUERY)
+	return (
+		<ThemeProvider theme={Theme}>
+			<>
+				<GlobalStyles />
+				<Router isLoggedIn={isLoggedIn} />
+			</>
+		</ThemeProvider>
+	)
+}
